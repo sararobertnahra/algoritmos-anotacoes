ed5f48d24c3a798ad71301f04b7e2f10b43ffa3a
Caio Augusto Palma <caio.palma@fatec.sp.gov.br>
2023-09-06 00:15:42 +0000

    Adiciona as notas de aula de algoritmos.

---
+++ algoritmos/fundamentos.py
@@ -0,0 +1,25 @@
+# Declaração de variáveis
+# Uma variável possui quatro características fundamentais: tipo, rótulo, valor e endereço de memória associado
+var_int = 10
+var_float = 10.1
+var_str = "str"
+var_bool = True
+print(type(var_int))
+print(type(var_float))
+print(type(var_str))
+print(type(var_bool))
+
+# Operações básicas
+# Operador = é o operador de atribuição. Ele atribui ao argumento da esquerda o valor da expressão à direita.
+soma = 1 + 1
+subtracao = 1 - 1
+multiplicacao = 1 * 1
+divisao_inteira = 1 / 2
+resto_divisaoA = divmod(10,3)
+resto_divisaoB = 10 % 3
+exponenciacao = 10 ** 2
+print(resto_divisaoB)
+
+# Importação de bibliotecas
+import math as matemagica
+matemagica.cos(45)
